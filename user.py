b5ff1f61982c56331a4af27d084928db31c5f8ff
markmurimi <murimimg180@gmail.com>
2018-04-01 22:13:35 +0300

    modified the user.py

---
+++ user.py
@@ -0,0 +1,14 @@
+class User:
+    '''This class generates a new user to the user_list'''
+    
+    user_list = []
+
+    def __init__(self,user_name,password);
+        self.user_name = user_name
+        self.password = password
+
+    def save_user(self):
+
+        '''This method saves the user details to the user_list'''
+
+        User.user_list.append(self)
