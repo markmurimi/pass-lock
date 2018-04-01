eb399251fd16de9a6dac8491e3e11ce01e1f7b5d
markmurimi <murimimg180@gmail.com>
2018-04-02 01:03:54 +0300

    modified the credentials

--- credentials.py
+++ credentials.py
@@ -1,1 +1,9 @@
-class Credentials
+class Credentials:
+
+    '''This is a class that comes up with the new instances for the credentials'''
+
+    def __init__(self, account_name, account_password):
+        self.account_name = account_name
+        self.account_password = account_password
+
+    credentials_list = []
