344f67aba2e23216110a969b368b6472adf1b5ae
markmurimi <murimimg180@gmail.com>
2018-04-02 12:00:20 +0300

    modified the test.py file

--- credentials-test.py
+++ credentials-test.py
@@ -24,11 +24,21 @@ class CredentialsTest(unittest.TestCase):
         self.assertEqual(self.new_credentials.account_name,"Google")
         self.assertEqual(self.new_credentials.account_password, "3344")
 
+    def test_find_credentials_by_name(self):
+        '''
+        test to find out if information is displayed
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("Google", "3344")
+        test_credentials.save_credentials()
+
     def test_delete_credentials(self):
         '''The deleting test'''
 
         self.new_credentials.save_credentials()
-        test_credentials = Credentials("Google","3344")test_credentials.save_credentials()
+        test_credentials = Credentials("Google","3344")
+        test_credentials.save_credentials()
         self.new_credentials.delete_credentials()
         self.assertEqual(len(Credentials.credentials_list), 1)
 
