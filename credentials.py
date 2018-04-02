db1584a4e2dcb0e2d5356a3dcc9a72ac282285db
markmurimi <murimimg180@gmail.com>
2018-04-02 12:00:52 +0300

    modified the py file

--- credentials.py
+++ credentials.py
@@ -6,4 +6,15 @@ class Credentials:
         self.account_name = account_name
         self.account_password = account_password
 
+    def delete_credentials(self):
+
+        '''Method used to delete the credentials'''
+
+        Credentials.credentials_list.remove(self)
+
+    def save_credentials(self):
+
+        '''The method that saves the credentials on the credentials list'''
+
+        self.credentials_list.append(self)
     credentials_list = []
