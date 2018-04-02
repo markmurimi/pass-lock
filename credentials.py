d16504f305fdfc75a05fd1dbd7380d5ce0dce735
markmurimi <murimimg180@gmail.com>
2018-04-02 13:11:37 +0300

    modified the py file to meet changes

--- credentials.py
+++ credentials.py
@@ -2,9 +2,17 @@ class Credentials:
 
     '''This is a class that comes up with the new instances for the credentials'''
 
+    credentials_list = []
+
     def __init__(self, account_name, account_password):
         self.account_name = account_name
         self.account_password = account_password
+
+    def save_credentials(self):
+        '''The method that saves the credentials on the credentials list'''
+
+        self.credentials_list.append(self)
+    
 
     def delete_credentials(self):
 
@@ -12,9 +20,4 @@ class Credentials:
 
         Credentials.credentials_list.remove(self)
 
-    def save_credentials(self):
-
-        '''The method that saves the credentials on the credentials list'''
-
-        self.credentials_list.append(self)
-    credentials_list = []
+    
